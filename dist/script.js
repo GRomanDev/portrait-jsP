cf8a2b0243a26f40707237b9917778be7c1565a3
Roman <grromf@gmail.com>
2022-02-03 22:39:33 +0800

    changed: animation of vert slider

--- dist/script.js
+++ dist/script.js
@@ -1118,7 +1118,7 @@ var sliders = function sliders(slides, dir, prev, next) {
     if (dir === "vertical") {
       paused = setInterval(function () {
         plusSlides(1);
-        items[slideIndex - 1].classList.add('slideInDown');
+        items[slideIndex - 1].classList.add('fadeInDown');
       }, 3000);
     } else {
       paused = setInterval(function () {
