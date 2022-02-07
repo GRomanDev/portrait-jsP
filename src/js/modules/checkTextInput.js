2dc7b4deb8805229321f1d1ecc607f75bdc5139e
Roman <grromf@gmail.com>
2022-02-07 13:55:29 +0800

    fixed:textInput only rus lang (autocomplite,paste)

--- src/js/modules/checkTextInput.js
+++ src/js/modules/checkTextInput.js
@@ -2,10 +2,8 @@ const checkTextInput = (selector) => {
   const txtInputs = document.querySelectorAll(selector);
 
   txtInputs.forEach(input => {
-    input.addEventListener('keypress', function (e) {
-      if (e.key.match(/[^а-яё 0-9]/ig)) {
-        e.preventDefault();
-      }
+    input.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/[A-Za-z]/g, '');
     });
   });
 };
